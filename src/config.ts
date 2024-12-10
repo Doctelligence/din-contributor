cbbb5d7ba151ef9933668babdf6e9aa241e74186
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-12-10 15:49:14 +0000

    fix: make sure to access sepolia chain

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,10 @@ import { mainnet, sepolia } from 'wagmi/chains'
 
 // @ts-ignore
 export const config = createConfig({
-  chains: [mainnet, sepolia],
+  chains: [sepolia],
   transports: {
-    [mainnet.id]: http(),
     [sepolia.id]: http(),
   },
 });
 
+export const CONTRACT_ADDRESS = '0x085b721dA12D70115B275174743dF90A1E3e44B4';
